3c839b135b7414ae840ba1c44cf62b6820d34ee2
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-11-04 19:55:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
-
-import pandas as pd
-import folium
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import folium
+
 
 # Membaca data
 all_data = pd.read_csv('all_data/data_grouped.csv')
