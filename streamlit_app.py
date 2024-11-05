16a3116285e77bc953a3f2755fd52ed55e509582
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-11-05 08:57:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from streamlit_folium import st_folium
 
 
 # Membaca data
-all_data = pd.read_csv('all_data/data_grouped.csv')
+all_data = pd.read_csv('all_data/AQI_all_data.csv')
 
 # Membuat DataFrame
 df_data = pd.DataFrame(all_data)
